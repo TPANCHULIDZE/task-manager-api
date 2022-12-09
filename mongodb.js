2fb95c9389fce2ca897de05314184aa54e35bb9b
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-12-09 11:44:38 +0400

    start

---
+++ mongodb.js
@@ -0,0 +1,24 @@
+const {MongoClient, ObjectId}= require("mongodb");
+
+
+const connectionURL =
+  "mongodb+srv://tato123456:vV0FHpCadatqpRS8@cluster0.ov0wgvy.mongodb.net/task-maneger";
+const databaseName = "task-maneger";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database");
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('tasks').deleteOne({description: 'second task'}).then(result => {
+      console.log(result);
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+);
